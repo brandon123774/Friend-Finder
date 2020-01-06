bce9db27dc0d3b7c7b5b574a93f6c5132872aac9
Brandon Shankar <bshankar12@comcast.net>
2020-01-05 20:33:40 -0800

    added to friends.js

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,58 @@
+//Data for all of the friends names and info
+friendsArray = [
+    {
+        friendName: "Tommy",
+        friendImage: "img",
+        scores: [
+            2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2,
+                2
+        ]
+    },
+    {
+        friendName: "Robert",
+        friendImage: "img",
+        scores: [
+            5,
+                5,
+                5,
+                5,
+                5,
+                5,
+                4,
+                5,
+                5,
+                5,
+                4
+        ]
+    },
+    {
+    friendName: "Julian",
+    friendImage: "img",
+    scores: [
+        3,
+            2,
+            3,
+            2,
+            3,
+            5,
+            3,
+            3,
+            1,
+            2,
+            3
+    ]
+
+    }
+];
+
+//export the file 
+module.exports = friendsArray;
--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,6 +1,8 @@
 //dependencies used/needed
 
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 // use the express server
 var app = express();
@@ -12,13 +14,18 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
 // require the hmtl and api routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// var apiRoutes = require("./routes/apiRoutes")
-// console.log(apiRoutes); //=> [Function]
-// apiRoutes(app);
+var apiRoutes = require("./routes/apiRoutes")
+console.log(apiRoutes); //=> [Function]
+apiRoutes(app);
 
 //set up the listen function to start up the server
 

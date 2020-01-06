7fea7958be7eca9d2256b9896f40dbe9ec3d8eaa
Brandon Shankar <bshankar12@comcast.net>
2020-01-05 21:07:50 -0800

    added to the apiRoutes

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -4,34 +4,49 @@ var friends = require("../data/friends.js");
 console.log(friends);
 
 //exports
-module.exports = function(app) {
+module.exports = function (app) {
 
-//api GET request with console log to see 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-    console.log("friend[0].scores[0]:" + friends[0].scores[0]);
-  });
+    //api GET request with console log to see 
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+        console.log("friend[0].scores[0]:" + friends[0].scores[0]);
+    });
 
-  //api POST request
-//   app.post("/api/friends", function(req, res) {
-  
-//     if (friends.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     }
-//     else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
+    //api POST request
+    app.post("/api/friends", function (req, res) {
 
+        // setup the array hoding the user's answers
+        var surveyResults = req.body.scores;
+        // convert the values in surveyResults to integer values
+        for (var i = 0; i < surveyResults.length; i++) {
+            surveyResults[i] = parseInt(surveyResults[i]);
+        }
+        //create variables to show the difference in the friends array and then another variable for the best match possible
+        var friendDifference = 999;
+        var bestMatch = 0;
 
-//api PUT request to clear the data if necessary
-  app.put("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = [];
-    waitListData.length = [];
+        //iterate through the friends array to get the selection and then show the best match available
+        for (i = 0; i < friends.length; i++) {
+            var theDifferencce = difference(surveyResults, friends[i].scores);
+            if (theDifference < friendDifference) {
+                friendDifference = theDifferencce;
+                bestMatch = i;
+            }
+        }
+            function difference(array1, array2) {
 
-    res.json({ ok: true });
-  });
+                // differenceAmount holds the tally of the difference between array values
+                var differenceAmount = 0;
+
+                for (var i = 0; i < array1.length; i++) {
+                    differenceAmount += Math.abs(array1[i] - array2[i]);
+                }
+
+                // return the difference between the two arrays reflecting the deviation
+                return differenceAmount;
+            }
+
+            // send the bestMatch back to the html page in response to the post
+            res.send(friends[bestMatch]);
+        });
 };
